84700747af334c115d62ba39abb42b8a4310758c
Rizky Syazuli <br4inwash3r@gmail.com>
2016-02-25 01:07:57 +0800

    general folder structure change to accomodate a live build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,29 @@ var gutil = require('gulp-util');
 var gulpSequence = require('gulp-sequence');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
+var wiredep = require('wiredep').stream;
 var browserSync = require('browser-sync');
 
 var prod = gutil.env.prod;
+
+gulp.task('html', function() {
+    return gulp.src('template/**/*.html')
+        // .pipe(prod ? processhtml() : gutil.noop())
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('bower', function() {
+    return gulp.src(['template/index.html'])
+        .pipe(wiredep())
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.stream());
+});
 
 gulp.task('sass', function() {
     return gulp.src('scss/**/*.scss')
@@ -27,21 +41,22 @@ gulp.task('sass', function() {
             browsers: ['last 2 versions', 'ie 9'],
             cascade: false
         }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('build/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function() {
     browserSync.init({
-        proxy: 'http://localhost/~rzky/templates/base-foundation/',
+        proxy: 'http://localhost/~rzky/templates/base-foundation/build/',
         watchTask: true,
         port: 8000
     });
 
+    gulp.watch('bower.json', ['bower']);
     gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('template/**/*.html', ['html']);
 });
 
 // use gulp-sequence to finish building html, sass and js before first page load
-gulp.task('default', gulpSequence(['sass'], 'serve'));
+gulp.task('default', gulpSequence(['html', 'bower', 'sass'], 'serve'));
