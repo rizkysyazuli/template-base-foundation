df631a19cd8ddd8d21060f5442cac4775538b718
Rizky Syazuli <br4inwash3r@gmail.com>
2018-07-23 11:08:21 +0700

    install linters

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+
+    },
+    "globals": {
+        "$": true,
+        "ga": true
+    }
+};
